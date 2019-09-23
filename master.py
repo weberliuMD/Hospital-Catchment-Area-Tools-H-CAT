870b10a1e06269291980ee2bc7667a7259c62fab
Weber Liu <54257955+weberliuMD@users.noreply.github.com>
2019-09-23 20:39:34 +1000

    Update master.py

--- master.py
+++ master.py
@@ -1,2 +1,16 @@
 import datasetPrep as dsp
-data = dsp.dataLoader("./Admissions_data/HTD_Original.csv")
+
+#Start the master function
+print("Where is the Original data file?")
+print("Ensure that you do not enter the location with quotation marks")
+print("Ensure also that there is only one header line")
+fileLocation = input()
+
+#load the file
+data = dsp.dataLoader(fileLocation)
+
+print("What is the column name for the patient ID?")
+IDCol = input()
+print("What is the column name for the patient address?")
+addressCol = input()
+
