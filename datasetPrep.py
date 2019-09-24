b794679f0ea3db66178f4de43e7973ec806a95a2
Weber Liu <54257955+weberliuMD@users.noreply.github.com>
2019-09-24 23:32:15 +1000

    Update datasetPrep.py

--- datasetPrep.py
+++ datasetPrep.py
@@ -3,10 +3,10 @@ from dsp import address_col as a_c
 from dsp import save_file as s_f
 
 def load_data (fileDir):
-    l_d.load_data(fileDir)
+    return l_d.load_data(fileDir)
 
 def address_col(data, columns):
-    a_c.address_col(data, columns)
+    return a_c.address_col(data, columns)
 
 def save_file(data, fileName, columns):
-    s_f.save_file(data, fileName, columns)
+    return s_f.save_file(data, fileName, columns)
