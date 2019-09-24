ba550a7d8280dfa97021c55c14bc9798a9327b9b
Weber Liu <54257955+weberliuMD@users.noreply.github.com>
2019-09-25 03:41:24 +1000

    Update gmaps_geocode.py

--- dsg/gmaps_geocode.py
+++ dsg/gmaps_geocode.py
@@ -1,5 +1,6 @@
 import urllib3
 import json
+import pandas as pd
 
 def gmaps_geocode(Address, APIKEY):
     gmapsREST = "https://maps.googleapis.com/maps/api/geocode/json?address="
@@ -7,11 +8,25 @@ def gmaps_geocode(Address, APIKEY):
     print("Request sent to "+ webAddress)
     http = urllib3.PoolManager()
     r = http.request('GET', webAddress)
-    response = json.loads(r.data.decode('utf-8'))
+    r = json.loads(r.data.decode('utf-8'))
     # print(respData)
-    if response['status'] == 'OK':
-        
-        
-        return response
+    if r['status'] == 'OK':
+        formattedAddress = r['results'][0]['formatted_address'] #formattedAddress is a str
+        address = r['results'][0]['address_components'] #address is a list of dicts
+        geometry = r['results'][0]['geometry']['location'] #geometry is a duct with only lat and long     
+        df = pd.DataFrame(address)
+        numElements = df.shape[0]
+        df = df.drop(['short_name'], axis = 1)
+        for i in range(numElements):
+            df['types'][i]= ''.join(df['types'][i])
+        df = df.T
+        df.columns = df.iloc[1]
+        df = df.drop(['types'], axis = 0)
+        df = df.assign(formatted_address = [formattedAddress])
+        df = df.assign(latitude = [geometry['lat']])
+        df = df.assign(longitude = [geometry['lng']])
+        df = df.reset_index()
+        df = df.drop(['index'], axis=1)
+        return df
     else:
-        return
+        return "missing"
