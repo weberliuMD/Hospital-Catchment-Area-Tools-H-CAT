28c8ee7dd9304b38ed9b7bda9eacdde43f3c920d
Weber Liu <54257955+weberliuMD@users.noreply.github.com>
2019-09-25 04:23:20 +1000

    gmaps geocode

--- master_dsg.py
+++ master_dsg.py
@@ -16,5 +16,10 @@ for i in range(height):
     geocodedData = dsg.gmaps_geocode(data.iloc[i][1])
     combinedData = pd.concat([od, geocodedData], axis=1)
     geocodedOutput = geocodedOutput.append(combinedData, ignore_index = True)
-    print(geocodedOutput)
+    print("Completed row", i)
+    export_csv = geocodedOutput.to_csv(r'./Geocoded_output_gmaps.csv', index = False, header=True, encoding='utf_8_sig')
+    if (i % 200 == 0):
+        fileName = "./Geocoded_output_gmaps_" + str(i) + "_PROGRESS.csv"
+        export_csv = geocodedOutput.to_csv(fileName, index = False, header=True, encoding='utf_8_sig')
+
 
