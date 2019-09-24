89de7aa49f2c70d0c50ae3ea12769c0aa1042964
Weber Liu <54257955+weberliuMD@users.noreply.github.com>
2019-09-25 02:29:15 +1000

    gmaps geocode

--- datasetgeocode.py
+++ datasetgeocode.py
@@ -1,15 +1,15 @@
 #Import Geocoding API scripts
-from dsg import ArcGIS_geocode as arcgis
-from dsg import bingmaps_geocode as bing
+# from dsg import ArcGIS_geocode as arcgis
+# from dsg import bingmaps_geocode as bing
 from dsg import gmaps_geocode as gmaps
-from dsg import here_geocode as here
-from dsg import mapquest_geocode as mapquest
-from dsg import OSM_geocode as osm
-from dsg import tomtom_geocode as tomtom
-from dsg import yahoo_geocode as yahoo
+# from dsg import here_geocode as here
+# from dsg import mapquest_geocode as mapquest
+# from dsg import OSM_geocode as osm
+# from dsg import tomtom_geocode as tomtom
+# from dsg import yahoo_geocode as yahoo
 
 #Import API Keys
 import keys
 
-def gmaps_geocode(Address, APIKEY=keys.GMAPS_API_KEY,):
+def gmaps_geocode(Address, APIKEY=keys.GMAPS_APIKEY,):
     return gmaps.gmaps_geocode(Address, APIKEY)
--- dsg/gmaps_geocode.py
+++ dsg/gmaps_geocode.py
@@ -1,3 +1,17 @@
+import urllib3
+import json
+
 def gmaps_geocode(Address, APIKEY):
-    
-    return Address
+    gmapsREST = "https://maps.googleapis.com/maps/api/geocode/json?address="
+    webAddress = gmapsREST + Address + "&key=" + APIKEY
+    print("Request sent to "+ webAddress)
+    http = urllib3.PoolManager()
+    r = http.request('GET', webAddress)
+    response = json.loads(r.data.decode('utf-8'))
+    # print(respData)
+    if response['status'] == 'OK':
+        
+        
+        return response
+    else:
+        return
