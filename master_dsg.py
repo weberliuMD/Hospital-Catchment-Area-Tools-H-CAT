ea914a5bcf62e025a75076d56bfa5782b4399074
Weber Liu <54257955+weberliuMD@users.noreply.github.com>
2019-09-25 04:31:17 +1000

    Update master_dsg.py

--- master_dsg.py
+++ master_dsg.py
@@ -1,25 +1,27 @@
 import datasetgeocode as dsg
-import pandas as pd
+dsg.gmaps_batch_geocode("Admissions_data/HTD_Trim.csv")
 
-data = dsg.load_data("./Admissions_data/HTD_Trim.csv")
+# import pandas as pd
 
-height = data.shape[0]
+# data = dsg.load_data("./Admissions_data/HTD_Trim.csv")
 
-geocodedOutput = pd.DataFrame()
+# height = data.shape[0]
 
-print(height)
-for i in range(height):
-    originalData = data.iloc[i]
-    od = pd.DataFrame(originalData).T
-    od = od.reset_index()
-    od = od.drop(['index'], axis=1)
-    geocodedData = dsg.gmaps_geocode(data.iloc[i][1])
-    combinedData = pd.concat([od, geocodedData], axis=1)
-    geocodedOutput = geocodedOutput.append(combinedData, ignore_index = True)
-    print("Completed row", i)
-    export_csv = geocodedOutput.to_csv(r'./Geocoded_output_gmaps.csv', index = False, header=True, encoding='utf_8_sig')
-    if (i % 200 == 0):
-        fileName = "./Geocoded_output_gmaps_" + str(i) + "_PROGRESS.csv"
-        export_csv = geocodedOutput.to_csv(fileName, index = False, header=True, encoding='utf_8_sig')
+# geocodedOutput = pd.DataFrame()
+
+# print(height)
+# for i in range(height):
+#     originalData = data.iloc[i]
+#     od = pd.DataFrame(originalData).T
+#     od = od.reset_index()
+#     od = od.drop(['index'], axis=1)
+#     geocodedData = dsg.gmaps_geocode(data.iloc[i][1])
+#     combinedData = pd.concat([od, geocodedData], axis=1)
+#     geocodedOutput = geocodedOutput.append(combinedData, ignore_index = True)
+#     print("Completed row", i)
+#     export_csv = geocodedOutput.to_csv(r'./Geocoded_output_gmaps.csv', index = False, header=True, encoding='utf_8_sig')
+#     if (i % 200 == 0):
+#         fileName = "./Geocoded_output_gmaps_" + str(i) + "_PROGRESS.csv"
+#         export_csv = geocodedOutput.to_csv(fileName, index = False, header=True, encoding='utf_8_sig')
 
 
