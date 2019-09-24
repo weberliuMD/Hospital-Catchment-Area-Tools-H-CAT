bbf894efccf7116a104a97dc29636894cdaeabea
Weber Liu <54257955+weberliuMD@users.noreply.github.com>
2019-09-25 04:09:51 +1000

    Update master_dsg.py

--- master_dsg.py
+++ master_dsg.py
@@ -3,4 +3,14 @@ import pandas as pd
 
 data = dsg.load_data("./Admissions_data/HTD_Trim.csv")
 
-print(dsg.gmaps_geocode(data.iloc[1][1]))
+height = data.shape[0]
+print(height)
+for i in range(height):
+    originalData = data.iloc[1]
+    od = pd.DataFrame(originalData).T
+    od = od.reset_index()
+    od = od.drop(['index'], axis=1)
+    geocodedData = dsg.gmaps_geocode(data.iloc[1][1])
+    combinedData = pd.concat([od, geocodedData], axis=1)
+    print(combinedData)
+
