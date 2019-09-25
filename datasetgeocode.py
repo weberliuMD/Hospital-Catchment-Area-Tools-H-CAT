16e7674ff9c754269bb28c18c5bd70680820096f
Weber Liu <54257955+weberliuMD@users.noreply.github.com>
2019-09-25 14:56:35 +1000

    Update datasetgeocode.py

--- datasetgeocode.py
+++ datasetgeocode.py
@@ -3,7 +3,7 @@
 # from dsg import bingmaps_geocode as bing
 from dsg import gmaps_geocode as gmaps
 # from dsg import here_geocode as here
-# from dsg import mapquest_geocode as mapquest
+from dsg import mapquest_geocode as mq
 from dsg import OSM_geocode as osm
 # from dsg import tomtom_geocode as tomtom
 # from dsg import yahoo_geocode as yahoo
