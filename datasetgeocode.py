b97cea64e0ca47499a28190713b5f0520d1514bf
Weber Liu <54257955+weberliuMD@users.noreply.github.com>
2019-09-25 15:19:34 +1000

    Update datasetgeocode.py

--- datasetgeocode.py
+++ datasetgeocode.py
@@ -155,7 +155,7 @@ def OSM_batch_geocode(fileLocation, saveProgress = 200, startPos = 0, endPos = N
     if (endPos == None):
         endPos = height
         print("end position is", height)
-    for i in range(startPos, height):
+    for i in range(startPos, endPos):
         originalData = data.iloc[i]
         od = pd.DataFrame(originalData).T
         od = od.reset_index()
@@ -173,3 +173,73 @@ def OSM_batch_geocode(fileLocation, saveProgress = 200, startPos = 0, endPos = N
         except:
             print("Something went wrong, error was caught, moving on...")
         time.sleep(1)
+
+def mapquest_geocode(Address, APIKEY=keys.MAPQUEST_APIKEY):
+    '''
+    mapquest_geocode(Address, APIKEY) is a function developed to query the mapquest (by Verizon) geocoding API.
+    This function takes in the argument of:
+    Address: The address of the patient, spaces allowed - fuzzy matching using mapquest's Algorithm
+    will clarify any issues. Provide as much detail as possible in the address.
+
+    The mapquest REST-API will return a JSON dataset, and from that, this application will determine if the address
+    were entered correctly (i.e. status 200 OK), and then interpret the data into:
+    - Formatted Address with various degrees of administrative areas, along with postal codes
+    - Geometry (latitude and longitude)
+    - mapUrl - map image of the location
+    - MapQuest linkID, geocodeQuality and geocodeQualityCode
+    - A status of whether or not retrieval was complete (response will be 0, regardless of whether or not it was properly geocoded)
+    This data is then returned as a DataFrame to the user.
+
+    Author: Weber Liu
+    Date Created: 25/09/2019
+    '''
+    return mq.mapquest_geocode(Address, APIKEY)
+
+def mapquest_batch_geocode(fileLocation, saveProgress = 200, APIKEY=None, startPos = 0, endPos = None):
+    ''''
+    mapquest_batch_geocode(fileLocation, saveProgress = 200, APIKEY=None) is a function developed to query the mapquest geocoding API.
+    This function takes in the argument of:
+    fileLocation - the location of the .csv file in the following format:
+    ------------------------------------------
+    |Patient identifier | Patient address    |
+    ------------------------------------------
+    |ID_001             | 123 Address st, ...|
+    (etc...)
+    ------------------------------------------
+    saveProgress - the number of geocodes conducted before a progress file is saved - this will occur alongside the live-save files
+    APIKEY - if an APIKEY is provided, it will be used. Otherwise, the APIKEY in keys.py will be used.
+
+    This batch geocoder has the dependency of the mapquest_geocode() function within this module.
+
+    Author: Weber Liu
+    Date Created: 25/09/2019
+    '''
+    data = load_data(fileLocation)
+    height = data.shape[0]
+    if (startPos == 0):
+        geocodedOutput = pd.DataFrame()
+    else:
+        geocodedOutput = pd.read_csv("./Geocoded_output_mapquest.csv")
+    if (endPos == None):
+        endPos = height
+        print("end position is", height)
+    for i in range(startPos, endPos):
+        originalData = data.iloc[i]
+        od = pd.DataFrame(originalData).T
+        od = od.reset_index()
+        od = od.drop(['index'], axis=1)
+        try:
+            geocodedData = None
+            if APIKEY==None:
+                geocodedData = mapquest_geocode(data.iloc[i][1])
+            else:
+                geocodedData = mapquest_geocode(Address=data.iloc[i][1], APIKEY = APIKEY)
+            combinedData = pd.concat([od, geocodedData], axis=1)
+            geocodedOutput = geocodedOutput.append(combinedData, ignore_index = True, sort=False)
+            print("Completed row", i)
+            export_csv = geocodedOutput.to_csv('./Geocoded_output_mapquest.csv', index = False, header=True, encoding='utf_8_sig')
+            if (i % saveProgress == 0):
+                fileName = "./Geocoded_output_mapquest_" + str(i) + "_PROGRESS.csv"
+                export_csv = geocodedOutput.to_csv(fileName, index = False, header=True, encoding='utf_8_sig')
+        except:
+            print("Something went wrong, error was caught, moving on...")
