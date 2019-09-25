d2b88067f92f9aea126e6adfbbe9a2a82e4de181
Weber Liu <54257955+weberliuMD@users.noreply.github.com>
2019-09-25 15:19:36 +1000

    Update master_dsg.py

--- master_dsg.py
+++ master_dsg.py
@@ -1,3 +1,4 @@
 import datasetgeocode as dsg
 # dsg.gmaps_batch_geocode("Admissions_data/HTD_Trim.csv", endPos = 1800)
-dsg.OSM_batch_geocode("Admissions_data/HTD_Trim.csv", endPos = 1000)
+# dsg.OSM_batch_geocode("Admissions_data/HTD_Trim.csv", endPos = 1000)
+dsg.mapquest_batch_geocode("Admissions_data/HTD_Trim.csv", endPos = 1000)
