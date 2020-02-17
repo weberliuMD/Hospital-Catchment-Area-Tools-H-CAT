9902de703dbda865b0fc67f848d7ab76b7876f9b
Weber Liu <54257955+weberliuMD@users.noreply.github.com>
2020-02-18 07:51:08 +1100

    updated because of failed API calls previous night

--- PNT_Cross_Geocode/bing-google/local_master_dsg.py
+++ PNT_Cross_Geocode/bing-google/local_master_dsg.py
@@ -1,4 +1,4 @@
 import sys
 sys.path.append('../..')
 import datasetgeocode as dsg
-dsg.gmaps_batch_geocode("../../PNT_Formatted_Address/PNT_bing.csv")
+dsg.gmaps_batch_geocode("../../PNT_Formatted_Address/PNT_bing.csv", startPos = 19400)
--- master_dsg.py
+++ master_dsg.py
@@ -1,8 +1,8 @@
 import datasetgeocode as dsg
 # dsg.gmaps_batch_geocode("./PNT_Formatted_Address/PNT_bing.csv")
 # dsg.OSM_batch_geocode("./HTD_Formatted_Address/HTD_bing.csv")
-# dsg.mapquest_batch_geocode("Admissions_data/HTD_Trim.csv", startPos = 9487, endPos = 12803)
+dsg.mapquest_batch_geocode("Admissions_data/HTD_Trim.csv", startPos = 12401, endPos = 12803)
 # dsg.bing_batch_geocode("./PNT_Formatted_Address/PNT_gmaps.csv")
 # dsg.here_batch_geocode("./PNT_Formatted_Address/PNT_gmaps.csv")
-dsg.tomtom_batch_geocode("Admissions_data/HTD_Trim.csv")
+# dsg.tomtom_batch_geocode("Admissions_data/HTD_Trim.csv")
 # dsg.tomtom_batch_fuzzysearch("Admissions_data/HTD_Trim.csv", startPos=1813)
